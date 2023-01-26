a1112a1fb2e1ca7025ca26514facc7c1d4f1299d
james <jameshanlonrcs@gmail.com>
2023-01-26 15:55:48 +0000

    updated to exercise 5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,32 +16,32 @@ fun printer(){
     val gender = "m"
     val employeeId = 6143
     val salary = 67543.21
-    val PAYE = 38.5 //percentage
-    val PRSI = 5.2 //percentage
+    val PAYE = .385 //percentage
+    val PRSI = .052 //percentage
     val bonus = 1450.50
     val scheme = 54.33
     val gross = (salary/12)+bonus/12
-    val totalDeductions = ((salary/12)*.385)+((salary/12)*.052)+scheme
+    val totalDeductions = ((salary/12)*PAYE)+((salary/12)*PRSI)+scheme
     val netpay = gross-totalDeductions
     val df = DecimalFormat("#.##")
 
-    println("________________________________________________________________________")
-    println("|                            Monthly Payslip                           |")
-    println("|______________________________________________________________________|")
-    println("|                                                                      |")
-    println("|      Employee Name: ${fName} ${lName}          Employee ID: ${employeeId}              |")
-    println("|                                                                      |")
-    println("|______________________________________________________________________|")
-    println("|                                                                      |")
-    println("|         PAYMENT DETAILS                  DEDUCTION DETAILS           |")
-    println("|______________________________________________________________________|")
-    println("|         Salary: ${df.format(salary/12)}                    PAYE: ${df.format((salary/12)*.385)}              |")
-    println("|         Bonus:  ${df.format(bonus/12)}                    PRSI:${df.format((salary/12)*.052)}                |")
-    println("|                                       Cycle To Work: ${df.format(scheme)}           |")
-    println("|______________________________________________________________________|")
-    println("|         Gross: ${df.format(gross)}                Total Deductions: ${df.format(totalDeductions)}      |")
-    println("|______________________________________________________________________|")
-    println("|                            NET PAY: ${df.format(netpay)}                          |")
-    println("|______________________________________________________________________|")
+    println("""    ________________________________________________________________________
+    |                            Monthly Payslip                           |
+    |______________________________________________________________________|
+    |                                                                      |
+    |      Employee Name: ${fName} ${lName}          Employee ID: ${employeeId}              |
+    |                                                                      |
+    |______________________________________________________________________|
+    |                                                                      |
+    |         PAYMENT DETAILS                  DEDUCTION DETAILS           |
+    |______________________________________________________________________|
+    |         Salary: ${df.format(salary/12)}                    PAYE: ${df.format((salary/12)*PAYE)}              |
+    |         Bonus:  ${df.format(bonus/12)}                    PRSI:${df.format((salary/12)*PRSI)}                |
+    |                                       Cycle To Work: ${df.format(scheme)}           |
+    |______________________________________________________________________|
+    |         Gross: ${df.format(gross)}                Total Deductions: ${df.format(totalDeductions)}      |
+    |______________________________________________________________________|
+    |                            NET PAY: ${df.format(netpay)}                          |
+    |______________________________________________________________________|""")
 
 }
